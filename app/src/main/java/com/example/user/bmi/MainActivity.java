ce388758d228a24f93d7070b112a0feedd9a3df8
User <kongyh-wa15@student.tarc.edu.my>
2017-10-28 09:16:44 +0800

    Initial

---
+++ app/src/main/java/com/example/user/bmi/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.user.bmi;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewBMI;
+    EditText editTextWeight, editTextHeight;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageBMI);
+        textViewBMI = (TextView)findViewById(R.id.resultText);
+        editTextWeight = (EditText)findViewById(R.id.weightText);
+        editTextHeight = (EditText)findViewById(R.id.heightText);
+    }
+
+    public void calculateBMI(View v){
+        String hString, wString;
+        double height, weight,bmi;
+
+        hString = editTextHeight.getText().toString();
+        wString = editTextWeight.getText().toString();
+        height = Double.parseDouble(hString);
+        weight = Double.parseDouble(wString);
+
+        bmi = weight/(height*height);
+        textViewBMI.setText(""+bmi);
+        if(bmi <= 18.5){
+            imageView.setImageResource(R.drawable.under);
+        }else if(bmi >= 25){
+            imageView.setImageResource(R.drawable.over);
+        }else{
+            imageView.setImageResource(R.drawable.normal);
+        }
+    }
+
+    public void reset(View v){
+        imageView.setImageResource(R.drawable.empty);
+        textViewBMI.setText("");
+        editTextHeight.setText("");
+        editTextWeight.setText("");
+    }
+}
